df24a252ed21502af97bb2a1793a4e261a2b30dd
JC Tapias <jctblock@gmail.com>
2022-11-07 14:00:58 -0500

    Checkpoint

--- DailyCode/CodingProblems/Problems/Week 2/task12.py
+++ DailyCode/CodingProblems/Problems/Week 2/task12.py
@@ -1,10 +1,10 @@
-def staircase(n, X):
+def staircase(x, y):
 
-    cache = [0 for _ in range(n + 1)]
+    cache = [0 for _ in range(x + 1)]
     cache[0] = 1
 
-    for i in range(n + 1):
-        cache[i] += sum(cache[i - x] for x in X if i - x > 0)
-        cache[i] += 1 if i in X else 0
-        
+    for i in range(x + 1):
+        cache[i] += sum(cache[i - x] for x in y if i - x > 0)
+        cache[i] += 1 if i in y else 0
+
     return cache[-1]
