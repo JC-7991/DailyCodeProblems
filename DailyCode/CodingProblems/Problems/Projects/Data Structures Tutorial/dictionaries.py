314c0520a4d7c6e7cb213a8a8ba5f219ac0c259c
JC <jctblock@gmail.com>
2022-11-14 15:38:43 -0500

    Checkpoint

--- DailyCode/CodingProblems/Problems/Projects/Data Structures Tutorial/dictionaries.py
+++ DailyCode/CodingProblems/Problems/Projects/Data Structures Tutorial/dictionaries.py
@@ -1,5 +1,5 @@
 # dictionaries have key-value pairs
-# dictionaries are unordered
+# dictionaries are unordered, thus elements cannot be ordered
 
 x = {'pork': 25.5, "beef": 34.5, "fish": 30}
 print(x)
@@ -10,4 +10,7 @@ print(x)
 x['pork'] = 100
 del(x['beef'])
 print(len(x))
+print(x)
+
+x.clear()
 print(x)
