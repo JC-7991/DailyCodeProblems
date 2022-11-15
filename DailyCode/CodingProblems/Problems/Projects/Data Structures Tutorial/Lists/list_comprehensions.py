f667ecaea8fa6b3ed2fd9f26e09013d45a9943f9
JC <jctblock@gmail.com>
2022-11-15 13:23:52 -0500

    Checkpoint

--- DailyCode/CodingProblems/Problems/Projects/Data Structures Tutorial/Lists/list_comprehensions.py
+++ DailyCode/CodingProblems/Problems/Projects/Data Structures Tutorial/Lists/list_comprehensions.py
@@ -3,3 +3,6 @@ import random
 # get values within a range
 under_10 = [x for x in range(10)]
 print('under_10: ' + str(under_10))
+
+squares = [x ** 2 for x in under_10]
+print('squares: ' + str(squares))
