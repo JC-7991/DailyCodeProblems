ba91c8a9296b89104699df7acdda6f71c5ec2acf
JC <jctblock@gmail.com>
2022-11-16 00:01:01 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions.py
@@ -2,7 +2,7 @@ import random
 
 # get values within a range
 under_10 = [x for x in range(10)]
-print('under_10: ' + str(under_10))
+print('under 10s: ' + str(under_10))
 
 # get squared values within a range
 squares = [x ** 2 for x in under_10]
