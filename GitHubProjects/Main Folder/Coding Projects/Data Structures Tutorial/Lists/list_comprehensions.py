b8bfe1e283fb216f767a8edea00ab1db0d7141ac
JC <jctblock@gmail.com>
2022-11-16 00:00:14 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions.py
@@ -7,3 +7,7 @@ print('under_10: ' + str(under_10))
 # get squared values within a range
 squares = [x ** 2 for x in under_10]
 print('squares: ' + str(squares))
+
+# get odd values
+odds = [x for x in range(10) if x % 2 == 1]
+print('odds: ' + str(odds))
