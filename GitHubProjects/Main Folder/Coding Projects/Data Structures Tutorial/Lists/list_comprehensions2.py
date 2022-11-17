a2896a102159d27702b9ac1a60207ceb82bbebda
JC <jctblock@gmail.com>
2022-11-17 12:03:43 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions2.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Lists/list_comprehensions2.py
@@ -2,3 +2,6 @@ s = "I love 2 go 2 the store 5 times a week."
 nums = [x for x in s if x.isnumeric()]
 print('nums: ' + ''.join(nums))
 
+names = ['Sonic', 'Tails', 'Knuckles', 'Shadow']
+idx = [k for k, v in enumerate(names) if v == 'Sonic']
+print('index = ' + str(idx[0]))
