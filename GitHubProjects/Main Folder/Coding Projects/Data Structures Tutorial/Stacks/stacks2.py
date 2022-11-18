f0410140274a76297b5527ea9f00b8a621974455
JC <jctblock@gmail.com>
2022-11-18 15:51:39 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks2.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks2.py
@@ -16,8 +16,11 @@ class Stack():
             return None
 
     def peek(self):
-        
+
         if len(self.stack) > 0:
             return self.stack[len(self.stack) - 1]
         else:
             return None
+    
+    def __str__(self):
+        pass
