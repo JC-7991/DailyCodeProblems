1e754964d77d556247e0c3939216494b5ac0abc9
JC <jctblock@gmail.com>
2022-11-18 13:47:01 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks2.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks2.py
@@ -2,6 +2,12 @@ class Stack():
 
     def __init__(self):
         self.stack = list()
-        
+
     def push(self, item):
         self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            return None
