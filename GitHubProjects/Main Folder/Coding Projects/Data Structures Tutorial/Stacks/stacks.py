e657e991fccac71aaf16f8cdb7a0b558555bd3cf
JC <jctblock@gmail.com>
2022-11-17 22:53:43 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks.py
@@ -1,3 +1,7 @@
 # stacks are LIFO (Last In, First Out)
 # all push/pop operations are to/from the top of the stack
 # peek - get item from the top of the stack without removing it
+
+# stack using list
+my_stack = list()
+print(my_stack)
