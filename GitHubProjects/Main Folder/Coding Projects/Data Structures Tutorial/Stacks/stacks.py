bb24ab2f78b283507f5956d5eb395674de0e6fc1
JC <jctblock@gmail.com>
2022-11-20 00:04:43 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Stacks/stacks.py
@@ -2,7 +2,7 @@
 # all push/pop operations are to/from the top of the stack
 # peek - get item from the top of the stack without removing it
 
-# stack using list
+# stack implementation using list
 my_stack = list()
 
 my_stack.append(4)
