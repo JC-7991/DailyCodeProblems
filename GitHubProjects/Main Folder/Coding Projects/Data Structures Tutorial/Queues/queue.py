cfcb5a0acb237e327aa3f58156b1f470476186d7
JC <jctblock@gmail.com>
2022-11-20 10:07:31 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Queues/queue.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Queues/queue.py
@@ -1,3 +1,7 @@
 # queue's are First-In, First-Out
 # enqueue - add item to the end of the line
 # dequeue - remove item from the front of the line
+
+from collections import deque
+
+my_queue = deque()
