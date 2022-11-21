1675e826c91bfbcc75d9596f37cc8b182f0dd86f
JC <jctblock@gmail.com>
2022-11-21 15:01:07 -0500

    Checkpoint

--- GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Queues/queue.py
+++ GitHubProjects/Main Folder/Coding Projects/Data Structures Tutorial/Queues/queue.py
@@ -1,12 +1,14 @@
 # queue's are First-In, First-Out
 # enqueue - add item to the end of the line
 # dequeue - remove item from the front of the line
+# a deque is a doubled-ended queue
 
 from collections import deque
 
 my_queue = deque()
 my_queue.append(5)
 my_queue.append(10)
+
 print(my_queue)
 print(my_queue.popleft())
 print(my_queue)
